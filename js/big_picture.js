20c8972d910b124e0b5578f4d076134207e81f5b
Nara Safarian <nara.safaryan@gmail.com>
2021-11-03 17:41:03 +0300

    Отрисуй меня полностью (часть 2)

---
+++ js/big_picture.js
@@ -0,0 +1,70 @@
+function openBigPicture (photo) {
+  const bigPicture = document.querySelector('.big-picture');
+
+  bigPicture.classList.remove('hidden');
+
+
+  bigPicture.querySelector('.big-picture__img img').src = photo.url;
+  bigPicture.querySelector('.likes-count').textContent = photo.likes;
+  bigPicture.querySelector('.comments-count').textContent = photo.comments.length;
+  bigPicture.querySelector('.social__caption').textContent = photo.description;
+
+
+  const commentsFragment = document.createDocumentFragment();
+  photo.comments.forEach((comment) => {
+    const commentElement = document.createElement('li');
+    commentElement.classList.add('social__comment');
+    const imageElement = document.createElement('img');
+    imageElement.classList.add('social__picture');
+    imageElement.setAttribute('src', comment.avatar);
+    imageElement.setAttribute('alt', comment.name);
+    imageElement.setAttribute('width', '35');
+    imageElement.setAttribute('height', '35');
+    commentElement.appendChild(imageElement);
+    const textElement = document.createElement('p');
+    textElement.classList.add('social__text');
+    textElement.textContent = comment.message;
+    commentElement.appendChild(textElement);
+  });
+
+  const socialCommentsElement = bigPicture.querySelector('.social__comments');
+  socialCommentsElement.appendChild(commentsFragment);
+
+
+  const socialCommentCount = bigPicture.querySelector('.social__comment-count');
+  socialCommentCount.classList.add('hidden');
+
+  const commentsLoader = bigPicture.querySelector('.comments-loader');
+  commentsLoader.classList.add('hidden');
+
+  document.body.classList.add('modal-open');
+}
+
+document.addEventListener('keydown', onKeyDown);
+
+
+const bigPictureCancel = document.querySelector('.big-picture__cancel');
+bigPictureCancel.addEventListener('click', onCloseBigPicture);
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+function onKeyDown (evt) {
+  evt.preventDefault();
+
+  if (isEscapeKey(evt)) {
+    closeBigPicture();
+  }
+}
+
+function closeBigPicture() {
+  const bigPicture = document.querySelector('.big-picture');
+  bigPicture.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+}
+
+function onCloseBigPicture (evt) {
+  evt.preventDefault();
+  closeBigPicture();
+}
+
+export {openBigPicture};
